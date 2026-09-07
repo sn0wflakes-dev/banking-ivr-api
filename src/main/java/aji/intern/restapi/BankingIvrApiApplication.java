6ebaedede4fca6938c3a53fcc1f7a9c7285f0eb0
Sn0wflakes <abdurrahmanali.dev@gmail.com>
2026-09-07 13:45:55 +0700

    init: initialize repo

---
+++ src/main/java/aji/intern/restapi/BankingIvrApiApplication.java
@@ -0,0 +1,13 @@
+package aji.intern.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingIvrApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankingIvrApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/aji/intern/restapi/BankingIvrApiApplicationTests.java
@@ -0,0 +1,13 @@
+package aji.intern.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingIvrApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
