c5f8579fa1a9766e93909810d91f8ee3a6d1f6d8
Sn0wflakes <abdurrahmanali.dev@gmail.com>
2026-09-07 14:03:04 +0700

    chore: add flake and gen classes using wsdl

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "ivr api for banking service flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+  };
+
+  outputs = {nixpkgs, ...}: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {inherit system; };
+  in {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        # insert desired packages
+      ];
+
+      shellHook = ''
+        echo "cleaning .env file"
+        sed -i '/^[[:space:]]*$/N;/^\n$/D' .env
+
+        echo "load .env file"
+        if [ -f .env ]; then
+            set -a
+            source .env
+            set +a
+            echo ".env file loaded successfully!"
+        else
+            echo "Could not load .env file. File not found"
+        fi
+        echo "dev env is fully set!"
+      '';
+    };
+  };
+}
