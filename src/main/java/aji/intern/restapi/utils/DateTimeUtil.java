c6db05d05b1e47d2b1abd32bad47cd8758d7901c
Sn0wflakes <abdurrahmanali.dev@gmail.com>
2026-09-23 19:33:57 +0700

    fix(util): change data type for transaction time using LocalTime and choose safer format for year (yyyy -> uuuu) and hour (hh -> HH)

--- src/main/java/aji/intern/restapi/utils/DateTimeUtil.java
+++ src/main/java/aji/intern/restapi/utils/DateTimeUtil.java
@@ -1,19 +1,20 @@
 package aji.intern.restapi.utils;
 
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
     public final ZoneId timezone = ZoneId.of("Asia/Jakarta");
-    public final DateTimeFormatter transactionTime = DateTimeFormatter.ofPattern("hh:mm:ss").withZone(timezone);
-    public final DateTimeFormatter transactionDate = DateTimeFormatter.ofPattern("dd-MM-yyyy").withZone(timezone);
+    public final DateTimeFormatter transactionTime = DateTimeFormatter.ofPattern("HH:mm:ss").withZone(timezone);
+    public final DateTimeFormatter transactionDate = DateTimeFormatter.ofPattern("dd-MM-uuuu").withZone(timezone);
 
     public DateTimeUtil() {}
 
     public String getTransactionTime() {
-        LocalDateTime date = LocalDateTime.now();
-        return date.format(transactionTime);
+        LocalTime time = LocalTime.now();
+        return time.format(transactionTime);
     }
 
     public String getTransactionDate() {
