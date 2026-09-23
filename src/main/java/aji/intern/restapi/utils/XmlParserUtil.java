fd2c5c0e126d9829c723c380fc49fcce2fa11e28
Sn0wflakes <abdurrahmanali.dev@gmail.com>
2026-09-23 19:08:57 +0700

    fix: add default response if parsing xml fault is fail

--- src/main/java/aji/intern/restapi/utils/XmlParserUtil.java
+++ src/main/java/aji/intern/restapi/utils/XmlParserUtil.java
@@ -17,26 +17,33 @@ public class XmlParserUtil {
     public static SoapFaultResponse xmlFaultParser(Detail detail) {
         try {
             Iterator<?> entries = detail.getDetailEntries();
-            if (entries.hasNext()) {
-                DetailEntry detailEntry = (DetailEntry) entries.next();
-                XPath xPath = XPathFactory.newInstance().newXPath();
 
-                String responseCode = xPath.evaluate("//*[local-name()='responseCode']", detailEntry);
-                String messageId = xPath.evaluate("//*[local-name()='messageId']", detailEntry);
-                String errorOrigin = xPath.evaluate("//*[local-name()='errorOrigin']", detailEntry);
-                String responseMessage = xPath.evaluate("//*[local-name()='responseMessage']", detailEntry);
-
-                return SoapFaultResponse.builder()
-                        .responseCode(responseCode)
-                        .messageId(messageId)
-                        .errorOrigin(errorOrigin)
-                        .responseMessage(responseMessage)
-                        .build();
+            if (!entries.hasNext()) {
+                return defaultSoapFault();
             }
+
+            DetailEntry detailEntry = (DetailEntry) entries.next();
+            XPath xPath = XPathFactory.newInstance().newXPath();
+
+            String responseCode = xPath.evaluate("//*[local-name()='responseCode']", detailEntry);
+            String messageId = xPath.evaluate("//*[local-name()='messageId']", detailEntry);
+            String errorOrigin = xPath.evaluate("//*[local-name()='errorOrigin']", detailEntry);
+            String responseMessage = xPath.evaluate("//*[local-name()='responseMessage']", detailEntry);
+
+            return SoapFaultResponse.builder()
+                    .responseCode(responseCode)
+                    .messageId(messageId)
+                    .errorOrigin(errorOrigin)
+                    .responseMessage(responseMessage)
+                    .build();
+
         } catch (Exception e) {
             log.error("Failed to parse xml for detail fault string, detail error : {}", e.getMessage());
+            return defaultSoapFault();
         }
+    }
 
+    private static SoapFaultResponse defaultSoapFault() {
         return SoapFaultResponse.builder()
                 .responseCode("N/A")
                 .messageId("N/A")
